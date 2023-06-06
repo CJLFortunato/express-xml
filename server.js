4f2b95eccf2a1ee7612e50d6c8b03d2e7bcdf604
CJL Fortunato <clemence.fortunato@gmail.com>
2023-06-06 17:00:50 +0200

    Init project

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(5000, () => console.log('Listening on port 5000...'));
