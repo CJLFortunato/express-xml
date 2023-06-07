389f2360a2aaf8de80c755d7fcca64c1c1e4ddf6
CJL Fortunato <clemence.fortunato@gmail.com>
2023-06-07 11:34:59 +0200

    Add schema for request body

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 import express from 'express';
+import Joi from 'joi';
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
+const schema = Joi.object({
+  Enseigne: Joi.string(),
+  Pays: Joi.string(),
+  NumPointRelais: Joi.string(),
+  Ville: Joi.string(),
+  CP: Joi.string(),
+  Latitude: Joi.string(),
+  Longitude: Joi.string(),
+  Taille: Joi.string(),
+  Poids: Joi.string(),
+  Action: Joi.string(),
+  DelaiEnvoi: Joi.string(),
+  RayonRecherche: Joi.string(),
+  TypeActivite: Joi.string(),
+  NACE: Joi.string(),
+  NombreResultats: Joi.number().integer(),
+  Security: Joi.string(),
+}). with('Pays', 'CP')
+
+app.post('/', (req, res) => {
+  const {
+    body
+  } = req;
+
+  res.send(body);
 
 });
 
