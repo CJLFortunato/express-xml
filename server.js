287197fdc3692da0c38faa2507dc2af5cd492810
CJL Fortunato <clemence.fortunato@gmail.com>
2023-06-07 14:51:15 +0200

    Finish schema

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
 import express from 'express';
 import Joi from 'joi';
 
+import {
+  schema
+} from './utils/schema.js';
+
 const app = express();
 app.use(express.json());
-
-const schema = Joi.object({
-  Enseigne: Joi.string(),
-  Pays: Joi.string(),
-  NumPointRelais: Joi.string(),
-  Ville: Joi.string(),
-  CP: Joi.string(),
-  Latitude: Joi.string(),
-  Longitude: Joi.string(),
-  Taille: Joi.string(),
-  Poids: Joi.string(),
-  Action: Joi.string(),
-  DelaiEnvoi: Joi.string(),
-  RayonRecherche: Joi.string(),
-  TypeActivite: Joi.string(),
-  NACE: Joi.string(),
-  NombreResultats: Joi.number().integer(),
-  Security: Joi.string(),
-}). with('Pays', 'CP')
 
 app.post('/', (req, res) => {
   const {
     body
   } = req;
 
-  res.send(body);
+  try {
+    Joi.assert(body, schema);
+    res.send(body);
+  } catch (e) {
 
+    res.status(422).send({
+      error: e.details[0].message
+    });
+  }
 });
 
 app.listen(5000, () => console.log('Listening on port 5000...'));
