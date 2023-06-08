cc61f7c95f8a849617d81fa8fb85f8d959996d13
bastien malgouyres <bmalgouyres@free.fr>
2023-06-08 15:42:53 +0200

    merge with xml to json

--- app/requests.js
+++ app/requests.js
@@ -1,7 +1,6 @@
 import { postRequest } from "./http-requests.js";
 import { jsonToXml } from "./jsonToXml.js"
 import { xmlToJson } from "./xmlToJson.js";
-const md5 = require("md5");
 
 function isNotErrorStatus(res) {
   console.log(res);
@@ -23,25 +22,4 @@ export async function postWSI4PointRelaisRecherche(body){
     const xmlBody = await jsonToXml(body)
     const result = await postRequest(xmlBody, urlSection)
     return isNotErrorStatus(result)
-}
-
-export async function jsonToXml(body) {
-  let body = req.body;
-
-  let securityKey = md5(
-    body.enseigne + body.pays + body.cp + body.privateKey
-  ).toUpperCase();
-
-  const xmlBody = `<?xml version="1.0" encoding="utf-8"?>
-                        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-                        <soap:Body>
-                            <WSI4_PointRelais_Recherche xmlns="http://www.mondialrelay.fr/webservice/">
-                            <Enseigne>${body.enseigne}</Enseigne>
-                            <Pays>${body.pays}</Pays>
-                            <CP>${body.cp}</CP>
-                            <Security>${securityKey}</Security>
-                            </WSI4_PointRelais_Recherche>
-                        </soap:Body>
-                        </soap:Envelope>`;
-  return xmlBody;
 }
