e57f4eaab87a94d4ffca30dab226c1374345061e
PintoLucas <81075866+PintoLucas@users.noreply.github.com>
2023-06-08 12:02:22 +0200

    Add JSON to XML conversion

--- app/requests.js
+++ app/requests.js
@@ -1,4 +1,6 @@
 import {postRequest} from "./http-requests.js";
+const md5 = require('md5');
+
 
 function isNotErrorStatus(res){
     if (typeof res != "number"){
@@ -13,6 +15,32 @@ function isNotErrorStatus(res){
 
 export async function postWSI4PointRelaisRecherche(body){
     const urlSection = "WSI4_PointRelais_Recherche"
-    // ToDo : add conversion json to xml here
+    // Conversion JSON to XML here
+    let body = req.body;
+
+    let securityKey = md5(body.enseigne 
+                            + body.pays 
+                            + body.cp
+                            + body.privateKey).toUpperCase();
+
+    const config = {
+        headers: {
+          'Content-Type': 'text/xml'
+        }
+      };
+    
+    const xmlBody = `<?xml version="1.0" encoding="utf-8"?>
+                        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                        <soap:Body>
+                            <WSI4_PointRelais_Recherche xmlns="http://www.mondialrelay.fr/webservice/">
+                            <Enseigne>${body.enseigne}</Enseigne>
+                            <Pays>${body.pays}</Pays>
+                            <CP>${body.cp}</CP>
+                            <Security>${securityKey}</Security>
+                            </WSI4_PointRelais_Recherche>
+                        </soap:Body>
+                        </soap:Envelope>`
+    
+    
     return isNotErrorStatus(await postRequest(body, urlSection))
 }
