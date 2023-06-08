ca41a07bb6cc18b3e508aa59dfd74a053a5b1a76
bastien malgouyres <bmalgouyres@free.fr>
2023-06-08 11:44:06 +0200

    add comments for connection

--- app/requests.js
+++ app/requests.js
@@ -3,6 +3,7 @@ import {postRequest} from "./http-requests.js";
 function isNotErrorStatus(res){
     if (typeof res != "number"){
         console.log("res not number : is xml")
+        // ToDo : add conversion xml to json here
         return res
     } else {
         console.log("res is number : is error status")
@@ -12,5 +13,6 @@ function isNotErrorStatus(res){
 
 export async function postWSI4PointRelaisRecherche(body){
     const urlSection = "WSI4_PointRelais_Recherche"
+    // ToDo : add conversion json to xml here
     return isNotErrorStatus(await postRequest(body, urlSection))
 }
