82ea6f8f93b5d13a3b998e42f4e9549268d5cdc9
CJL Fortunato <clemence.fortunato@gmail.com>
2023-06-08 15:23:36 +0200

    Correct private key

--- app/jsonToXml.js
+++ app/jsonToXml.js
@@ -2,10 +2,10 @@ import md5 from "md5" //const md5 = require('md5');
 
 export async function jsonToXml(body) {
 
-    let securityKey = md5(body.Enseigne 
-                            + body.Pays 
-                            + body.CP
-                            + body.privateKey).toUpperCase();
+    let securityKey = md5(body.Enseigne +
+        body.Pays +
+        body.CP +
+        "PrivateK").toUpperCase();
 
     const xmlBody = `<?xml version="1.0" encoding="utf-8"?>
                     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
