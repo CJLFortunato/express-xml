154e0810e86c0484a98fb0357ac023130f9d9e8d
bastien malgouyres <bmalgouyres@free.fr>
2023-06-08 11:38:57 +0200

    update then & catch + isNotErrorStatus()

--- app/requests.js
+++ app/requests.js
@@ -1,7 +1,16 @@
 import {postRequest} from "./http-requests.js";
 
+function isNotErrorStatus(res){
+    if (typeof res != "number"){
+        console.log("res not number : is xml")
+        return res
+    } else {
+        console.log("res is number : is error status")
+        return res
+    }
+}
+
 export async function postWSI4PointRelaisRecherche(body){
     const urlSection = "WSI4_PointRelais_Recherche"
-    const request = await postRequest(body, urlSection)
-    return request
+    return isNotErrorStatus(await postRequest(body, urlSection))
 }
